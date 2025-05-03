002d70f580f2918ef55d9a91b4935bcc03c66585
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-02 19:17:27 -0500

    feat: Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let stringStart = string.split("");
+    let reversedString = [];
+
+    for(let i = string.length - 1; i >= 0; i--){
+        reversedString.push(stringStart[i]);    
+    }
+
+    let newArray = reversedString.join('');
+
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
