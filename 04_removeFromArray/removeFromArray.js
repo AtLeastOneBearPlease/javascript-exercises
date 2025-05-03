263e4180e7cdf2e1bccadbc1a9718f0882bec58e
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-02 19:52:48 -0500

    feat: Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToCull, ...valuesToRemove) {
+
+    let culledArray = Array();
+
+    cullArray:
+    for(let i = 0; i < arrayToCull.length; i++){
+        for(let j = 0; j < valuesToRemove.length; j++){
+            if(arrayToCull[i] === valuesToRemove[j]) continue cullArray;
+        }
+    
+        culledArray.push(arrayToCull[i]);
+    }
+
+    return culledArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+
+Loop through the input array
+    Loop through the valuesToRemove array
+        IF any values in valuesToRemove equal the index at the input array
+            Don't add to culled array
+        ELSE
+            Add to culled array;
+Return culledArray
+*/
