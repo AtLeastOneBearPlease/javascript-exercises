4e017d0c35f72544da06e9a028b9eebfe4e8ebb7
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-02 20:27:16 -0500

    feat: Solved exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,26 @@
-const leapYears = function() {
+const FOUR_YEARS = 4;
+const CENTURY = 100;
+const FOUR_CENTURIES = 400;
+
+function divisibleByFourYears (year){
+    return year % FOUR_YEARS === 0;
+}
+
+function notDivisibleByOneCentury(year){
+    return year % CENTURY !== 0;
+}
+
+function divisibleByFourCenturies(year){
+    return year % FOUR_CENTURIES === 0;
+}
+
+const leapYears = function(year) {
+
+    if(divisibleByFourCenturies(year)) return true;
+
+    if(divisibleByFourYears(year) && notDivisibleByOneCentury(year)) return true;
+
+    return false;
 
 };
 
