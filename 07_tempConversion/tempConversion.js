466b7fa74010d683030aafa20e7c0a8ac933a273
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-05-03 10:11:26 -0500

    feat: Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundToTenths(number){
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function(farenheit) {
+  return roundToTenths( (farenheit - 32) * (5/9) );
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundToTenths(celsius * (9/5) + 32);
 };
 
 // Do not edit below this line
